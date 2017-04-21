963a083da13e9de2f620a4c90a4dcb9d7e076d84
Agustin <agus.luques@hotmail.com>
2017-04-21 12:51:08 -0300

    Arreglo un par de cosas que habia entendido mal

--- instalador.sh
+++ instalador.sh
@@ -6,7 +6,7 @@
 GRUPO=`pwd`"/Grupo01/"
 
 #Declaro subdirectorio dirconf (RESERVADO)
-DIRCONF="$GRUPO""dirconf"
+DIRCONF="$GRUPO""dirconf/"
 
 #Declaro subdirectorio ejecutable (DEFAULT)
 DIRBIN="$GRUPO""bin/"
@@ -31,6 +31,9 @@ DIRREP="$GRUPO""reportes/"
 
 #Declaro subdirectorio log (DEFAULT)
 DIRLOG="$GRUPO""log/"
+
+#PATH al archivo de config
+ARCHCONF="$DIRCONF""arch.conf"
 
 ##############################################################################
 
@@ -68,17 +71,204 @@ definirNombresDirectorios(){
 		echo "DEFAULT $DIRBIN"
 		echo
 		read INPUT
-		if [ "$INPUT" != "dirconf" ] && [ "$INPUT" != "" ] && [ ! -e "$GRUPO$INPUT/" ] ; then
+		if [ "$INPUT" != "dirconf" ] && [ ! -e "$GRUPO$INPUT/" ] ; then
 			CORTE=true
 			DIRBIN="$GRUPO""$INPUT""/"
+		elif [ -z "$INPUT" ]; then
+			CORTE=true
 		else
 			echo
 			echo "DIRECTORIO INCORRECTO!!!!!"
 			echo "Verifique que el nombre no sea: "
-			echo "Duplicado, vacio o igual a \"dircof\""
+			echo "Duplicado o igual a \"dircof\""
 			echo
 		fi
 
+	done
+
+	CORTE=false
+
+	while [ "$CORTE" = false ]; do
+
+		echo "DIRECTORIO PARA MAESTROS"
+		echo "DEFAULT $DIRMA"
+		echo
+		read INPUT
+		if [ "$INPUT" != "dirconf" ] && [ ! -e "$GRUPO$INPUT/" ] ; then
+			CORTE=true
+			DIRMA="$GRUPO""$INPUT""/"
+		elif [ -z "$INPUT" ]; then
+			CORTE=true
+		else
+			echo
+			echo "DIRECTORIO INCORRECTO!!!!!"
+			echo "Verifique que el nombre no sea: "
+			echo "Duplicado o igual a \"dircof\""
+			echo
+		fi
+
+	done
+
+	CORTE=false
+
+	while [ "$CORTE" = false ]; do
+
+		echo "DIRECTORIO PARA NOVEDADES"
+		echo "DEFAULT $DIRNOV"
+		echo
+		read INPUT
+		if [ "$INPUT" != "dirconf" ] && [ ! -e "$GRUPO$INPUT/" ] ; then
+			CORTE=true
+			DIRNOV="$GRUPO""$INPUT""/"
+		elif [ -z "$INPUT" ]; then
+			CORTE=true
+		else
+			echo
+			echo "DIRECTORIO INCORRECTO!!!!!"
+			echo "Verifique que el nombre no sea: "
+			echo "Duplicado o igual a \"dircof\""
+			echo
+		fi
+
+	done
+
+	CORTE=false
+
+	while [ "$CORTE" = false ]; do
+
+		echo "DIRECTORIO PARA ACEPTADOS"
+		echo "DEFAULT $DIRACE"
+		echo
+		read INPUT
+		if [ "$INPUT" != "dirconf" ] && [ ! -e "$GRUPO$INPUT/" ] ; then
+			CORTE=true
+			DIRACE="$GRUPO""$INPUT""/"
+		elif [ -z "$INPUT" ]; then
+			CORTE=true
+		else
+			echo
+			echo "DIRECTORIO INCORRECTO!!!!!"
+			echo "Verifique que el nombre no sea: "
+			echo "Duplicado o igual a \"dircof\""
+			echo
+		fi
+
+	done
+
+	CORTE=false
+
+	while [ "$CORTE" = false ]; do
+
+		echo "DIRECTORIO PARA RECHAZADOS"
+		echo "DEFAULT $DIRREJ"
+		echo
+		read INPUT
+		if [ "$INPUT" != "dirconf" ] && [ ! -e "$GRUPO$INPUT/" ] ; then
+			CORTE=true
+			DIRREJ="$GRUPO""$INPUT""/"
+		elif [ -z "$INPUT" ]; then
+			CORTE=true
+		else
+			echo
+			echo "DIRECTORIO INCORRECTO!!!!!"
+			echo "Verifique que el nombre no sea: "
+			echo "Duplicado o igual a \"dircof\""
+			echo
+		fi
+
+	done
+
+	CORTE=false
+
+	while [ "$CORTE" = false ]; do
+
+		echo "DIRECTORIO PARA VALIDADOS"
+		echo "DEFAULT $DIRVAL"
+		echo
+		read INPUT
+		if [ "$INPUT" != "dirconf" ] && [ ! -e "$GRUPO$INPUT/" ] ; then
+			CORTE=true
+			DIRVAL="$GRUPO""$INPUT""/"
+		elif [ -z "$INPUT" ]; then
+			CORTE=true
+		else
+			echo
+			echo "DIRECTORIO INCORRECTO!!!!!"
+			echo "Verifique que el nombre no sea: "
+			echo "Duplicado o igual a \"dircof\""
+			echo
+		fi
+
+	done
+
+	CORTE=false
+
+	while [ "$CORTE" = false ]; do
+
+		echo "DIRECTORIO PARA REPORTES"
+		echo "DEFAULT $DIRREP"
+		echo
+		read INPUT
+		if [ "$INPUT" != "dirconf" ] && [ ! -e "$GRUPO$INPUT/" ] ; then
+			CORTE=true
+			DIRREP="$GRUPO""$INPUT""/"
+		elif [ -z "$INPUT" ]; then
+			CORTE=true
+		else
+			echo
+			echo "DIRECTORIO INCORRECTO!!!!!"
+			echo "Verifique que el nombre no sea: "
+			echo "Duplicado o igual a \"dircof\""
+			echo
+		fi
+
+	done
+
+	CORTE=false
+
+	while [ "$CORTE" = false ]; do
+
+		echo "DIRECTORIO PARA LOGS"
+		echo "DEFAULT $DIRLOG"
+		echo
+		read INPUT
+		if [ "$INPUT" != "dirconf" ] && [ ! -e "$GRUPO$INPUT/" ] ; then
+			CORTE=true
+			DIRLOG="$GRUPO""$INPUT""/"
+		elif [ -z "$INPUT" ]; then
+			CORTE=true
+		else
+			echo
+			echo "DIRECTORIO INCORRECTO!!!!!"
+			echo "Verifique que el nombre no sea: "
+			echo "Duplicado o igual a \"dircof\""
+			echo
+		fi
+
+	done
+
+	CORTE=false
+
+	while [ "$CORTE" = false ]; do
+		echo "LOS VALORES INDICADOS SON: "
+		echo
+		echo "EJECUTABLES=$DIRBIN"
+		echo "MAESTROS=$DIRMA"
+		echo "NOVEDADES=$DIRNOV"
+		echo "ACEPTADOS=$DIRACE"
+		echo "RECHAZADOS=$DIRREJ"
+		echo "VALIDADOS=$DIRVAL"
+		echo "REPORTES=$DIRREP"
+		echo "LOGS=$DIRLOG"
+		echo
+		echo "Si desea editar alguno presione Editar. Si no, presione Ok."
+		read INPUT
+		if [ "$INPUT" = "Ok" ]; then
+			CORTE=true
+		elif [ "$INPUT" = "Editar" ]; then
+			definirNombresDirectorios
+			CORTE=true
+		fi
 	done
 
 
@@ -92,14 +282,18 @@ definirNombresDirectorios(){
 
 if [ "$1" = "-t" ]; then
 
-	if [ -s "$DIRCONF" ]; then
-		echo "Archivo $DIRCONF:"
+	if [ -e "$ARCHCONF" ]; then
+		echo "El sistema está instalado"
+		echo "Contenido del archivo $ARCHCONF:"
 		echo
-		cat "$DIRCONF"
+		cat "$ARCHCONF"
 		echo
+		echo
+		exit 0
 	else
 		echo "El sistema no está instalado"
 		echo
+		exit 1
 	fi
 	
 fi
@@ -108,6 +302,8 @@ chequeoPerl
 
 definirNombresDirectorios
 
+
+	
 
 
 
